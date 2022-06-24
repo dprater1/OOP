92b1aac2a876eea0e4efa096c5283cff014a15a5
Daivion <dprater@ucmerced.edu>
2022-06-23 23:45:05 -0700

    Implemented function to obtain URL and also class edit

--- Basketball.java
+++ Basketball.java
@@ -5,75 +5,64 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.jsoup.*;
-
+import java.util.Scanner;
 public class Basketball {
     public static void main(String args[]){
-
-        final String lebronurl = "https://www.basketball-reference.com/players/j/jamesle01.html";
-    
-            try {
-                final Document docu = Jsoup.connect(lebronurl).get();
-
-                for(Element row : docu.select("table#per_game > tfoot > tr")){
-                        final String ticker = row.select("[data-stat=pts_per_g]").text();
-                    System.out.println(ticker);
-                    break;
-                    }
-            }
-           catch (Exception ex){
-             ex.printStackTrace();
-            }
-           
-
-    }
-    private String team;
-    
-
-    public Basketball(){
-        this.team = "";
-       
-    }
-
-    public Basketball(String player){
-        this.team = "";
+        Scanner in = new Scanner(System.in);
+        System.out.println("What is the first name of the player you'd like to search?");
+        String first = in.nextLine();
+        System.out.println("What is the last name of the player you'd like to search?");
+        String last = in.nextLine();
+        
+        PlayerData data = new PlayerData(first, last);
         
     }
 
-    public void setTeam(String team){
-        this.team = team;
-    }
-    public String getTeam(){
-        return team;
-    }
-    
     
 
 }
 
-class Player extends Basketball{
-    
-   
-    private String name;
-    private int rings;
-    private int MVPs;
-    
-    public Player(){
-        this.name = "";
-        this.rings = 0;
-        this.MVPs = 0;
+class PlayerData{
+    private String firstName;
+    private String lastName;
+    public PlayerData(){
+        this.firstName = "";
+        this.lastName = "";
     }
-    public Player(String player){
-        this.name = player;
-        this.rings = 0;
-        this.MVPs = 0;
+    public PlayerData(String first, String last){
+        this.firstName = first;
+        this.lastName = last;
     }
 
-    public void setName(String name){
-        this.name = name;
+    public String PPG(){
+    final String lebronurl = getInfo(firstName, lastName);
+    
+    try {
+        final Document docu = Jsoup.connect(lebronurl).get();
+        //tr:eq(0)
+        for(Element row : docu.select("table#per_game > tfoot > tr:eq(0)")){
+                final String ticker = row.select("[data-stat=pts_per_g]").text();
+            System.out.println(ticker + " | ");
+            }
     }
+   catch (Exception ex){
+     ex.printStackTrace();
+    }
+    return "";
+}
 
-    public String getName(){
-        return name;
+    public String getInfo(String first, String last){
+        String baseURL = "https://www.basketball-reference.com/players/";
+        String lastN;
+        if(last.length() < 5) lastN = last.substring(0, last.length()).toLowerCase();
+        else lastN = last.substring(0,5).toLowerCase();
+        String lastLet = last.substring(0,1).toLowerCase();
+        String firsttwo = first.substring(0,2).toLowerCase();
+
+        String finalUrl = baseURL + lastLet + "/" + lastN + firsttwo + "01.html";
+
+        return finalUrl;
+
     }
 
 
